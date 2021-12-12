21074c8d6dfb07a671a9fed6ab3fe50bdb19365a
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-12 22:04:55 +0900

    Feet: Updated distinguish with categories(TO_DO,DOING,DONE)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,6 +1,41 @@
-import { IToDo } from "../atom";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atom";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}</li>;
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((prev) => {
+      const targetIndex = prev.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      const newToDos = [...prev];
+      newToDos.splice(targetIndex, 1, newToDo);
+      return newToDos;
+    });
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
+
 export default ToDo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,30 @@
-import { useRecoilValue } from "recoil";
-import { toDoState } from "../atom";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atom";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function TodoList() {
-  const toDos = useRecoilValue(toDoState);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setCategory(value);
+  };
+
   return (
     <>
       <div>
         <h1>To Dos</h1>
         <hr />
+        <select onInput={onInput} value={category}>
+          <option value="TO_DO">To Do</option>
+          <option value="DOING">Doing</option>
+          <option value="DONE">Done</option>
+        </select>
         <CreateToDo />
         <ul>
           {toDos.map((toDo) => (
